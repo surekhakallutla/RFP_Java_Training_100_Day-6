372317a552e62dc05418ac8dbbccffedcb1350c2
surekhakallutla <surekhakallutla@gmail.com>
2022-01-15 19:07:08 +0530

    Updating Day-6 Practice Problems

---
+++ src/com/bridgelabz/patterns/pattern1.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.patterns;
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		
+		int i,j,n=7;
+		
+		for(i=0; i<=n; i++)
+		{
+			for(j=0; j<i ; j++)
+			{
+								
+			System.out.print("*");
+			
+			}
+			
+		System.out.println("*");
+		}
+		
+		
+}
+}
---
+++ src/com/bridgelabz/patterns/pattern2hollowtriangle.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.patterns;
+
+public class pattern2hollowtriangle 
+{
+
+		public static void main(String[] args)
+		{
+			
+			int i,j;
+			int n = 7;
+			
+			for(i=1; i<=n; i++)
+			{
+				for(j=1 ; j<=i ; j++)
+				{
+					if(j==1 ||j==i || i==n )
+						System.out.print("* ");
+					else
+						System.out.print("  ");
+				
+				}
+				
+			System.out.println(" ");
+			}
+	
+	}
+}
